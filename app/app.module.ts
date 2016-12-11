f73cd5c5654b94c7de1f49e68f1dbd43c6f0b47f
Luke Schlangen <lukeschlangen@gmail.com>
2016-12-11 15:32:58 -0600

    Dashboard and hoeroes routes complete

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,14 +4,34 @@ import { FormsModule }    from '@angular/forms';
 import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroService }         from './hero.service';
+import { RouterModule }   from '@angular/router';
+
+
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
